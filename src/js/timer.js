9acb19da36d6f942535901f9f978609b7d629117
Ekaterina-Lavrinenko <lavrinenko.ey@gmail.com>
2021-05-23 00:17:43 +0100

    Добавляет таймер обратного отсчета

---
+++ src/js/timer.js
@@ -0,0 +1,69 @@
+import '../styles.css';
+
+const refs = {
+    timer: document.querySelector('#timer-1'),
+};
+
+const timer = {
+    finish() {
+        const finishTime = new Date('Jul 17, 2021');
+
+        setInterval(() => {
+            const currentTime = Date.now();
+            const deltaTime = finishTime - currentTime;
+            const time = getTimeComponents(deltaTime);
+            // console.log(currentTime - finishTime);
+            // console.log(`${days}:${hours}:${mins}:${secs}`);
+            updateClockface(time);
+        }, 1000);
+    },
+};
+
+// new CountdownTimer({
+// //   selector: '#timer-1',
+// //   targetDate: new Date('Jul 17, 2019'),
+// // });
+    
+timer.finish();
+
+function updateClockface({ days, hours, mins, secs }) {
+    refs.timer.textContent = `${days}:${hours}:${mins}:${secs}`;
+}
+
+// выводит значения таймера в двузначном виде
+function pad(value) {
+    return String(value).padStart(2, '0');
+}
+
+function getTimeComponents(time) {
+    /*
+ * Оставшиеся дни: делим значение UTC на 1000 * 60 * 60 * 24, количество
+ * миллисекунд в одном дне (миллисекунды * секунды * минуты * часы)
+ */
+const days = pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+
+/*
+ * Оставшиеся часы: получаем остаток от предыдущего расчета с помощью оператора
+ * остатка % и делим его на количество миллисекунд в одном часе
+ * (1000 * 60 * 60 = миллисекунды * минуты * секунды)
+ */
+const hours = pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
+
+/*
+ * Оставшиеся минуты: получаем оставшиеся минуты и делим их на количество
+ * миллисекунд в одной минуте (1000 * 60 = миллисекунды * секунды)
+ */
+const mins = pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+
+/*
+ * Оставшиеся секунды: получаем оставшиеся секунды и делим их на количество
+ * миллисекунд в одной секунде (1000)
+ */
+const secs = pad(Math.floor((time % (1000 * 60)) / 1000));
+
+    return { days, hours, mins, secs };
+}
+
+function updateClockface({ days, hours, mins, secs }) {
+    refs.timer.textContent = `${days}:${hours}:${mins}:${secs}`;
+}
